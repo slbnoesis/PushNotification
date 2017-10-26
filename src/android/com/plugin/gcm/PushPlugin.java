de02aa98c5223388ce4910ee7f48895c8c8ef077
zmoreira <zmoreira@users.noreply.github.com>
2017-10-26 16:32:07 +0100

    Update PushPlugin.java

--- src/android/com/plugin/gcm/PushPlugin.java
+++ src/android/com/plugin/gcm/PushPlugin.java
@@ -218,7 +218,7 @@ public class PushPlugin extends CordovaPlugin {
    * Sends a json object to the client as parameter to a method which is defined in gECB.
    */
 	public static void sendJavascript(JSONObject _json) {
-		String _d = "javascript:" + gECB + "(" + _json.toString() + ")";
+		String _d = "javascript:console.log('Debug Notificatios Push');" + gECB + "(" + _json.toString() + ")";
 		Log.v(TAG, "sendJavascript: " + _d);
 
 		if (gECB != null && gWebView != null) {
